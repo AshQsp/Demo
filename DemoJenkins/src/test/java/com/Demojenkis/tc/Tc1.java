d93a1537f48b543b36461f24f2299a89ab5d0965
HP <HP@ASISH>
2024-02-12 15:26:57 +0530

    Tc1 Done

--- DemoJenkins/src/test/java/com/Demojenkis/tc/Tc1.java
+++ DemoJenkins/src/test/java/com/Demojenkis/tc/Tc1.java
@@ -1,10 +1,13 @@
 package com.Demojenkis.tc;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Tc1 {
-	
+	@Test
 	public void tc1()
 	{
-		
+		Reporter.log("Tc1 Passsed",true);
 	}
 
 }
