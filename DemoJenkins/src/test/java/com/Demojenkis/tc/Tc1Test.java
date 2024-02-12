8ef6bd3a4c21dc28f4579398d77a513e85c9e30d
HP <HP@ASISH>
2024-02-12 15:36:11 +0530

    Eng ClassNameUpdate

---
+++ DemoJenkins/src/test/java/com/Demojenkis/tc/Tc1Test.java
@@ -0,0 +1,13 @@
+package com.Demojenkis.tc;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1Test {
+	@Test
+	public void tc1()
+	{
+		Reporter.log("Tc1 Passsed",true);
+	}
+
+}
---
+++ DemoJenkins/src/test/java/com/Demojenkis/tc/Tc2Test.java
@@ -0,0 +1,13 @@
+package com.Demojenkis.tc;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test {
+	@Test
+	public void tc2()
+	{
+		Reporter.log("Tc2 Passsed",true);
+	}
+
+}
