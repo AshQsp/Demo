e6589fc704c7055ef28c62fd037c0cfd2f50127d
HP <HP@ASISH>
2024-02-13 14:30:42 +0530

    Tc3 Updated

---
+++ DemoJenkins/src/test/java/com/Demojenkis/tc/Tc3.java
@@ -0,0 +1,14 @@
+package com.Demojenkis.tc;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3 {
+	
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Tc3 Passed",true);
+	}
+
+}
